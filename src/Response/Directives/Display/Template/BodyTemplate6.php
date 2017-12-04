630be4ce3a04df98c14c995d7100611803644b9e
Julian Meier <info@julians-info.de>
2017-12-04 23:36:17 +0100

    removed unused variable

--- src/Response/Directives/Display/Template/BodyTemplate6.php
+++ src/Response/Directives/Display/Template/BodyTemplate6.php
@@ -5,17 +5,12 @@ namespace Develpr\AlexaApp\Response\Directives\Dialog\Template;
 class BodyTemplate6 extends BaseTemplate
 {
     const TYPE = 'BodyTemplate6';
-	private $backgroundImage = null;
 	private $textContent = null;
 
     public function getType()
     {
         return $this::TYPE;
     }
-	
-	public function setBackgroundImage($description, $backgroundImage) {
-		$this->backgroundImage = new BackgroundImage($description, $backgroundImage);
-	}
 	
 	public function setTextContent($type, $text) {
 		if ($this->textContent == null) {
@@ -28,7 +23,6 @@ class BodyTemplate6 extends BaseTemplate
     public function toArray()
     {
         return array_merge([
-            'backgroundImage' => $this->backgroundImage->toArray(),
             'image' => 'Image',
             'textContent' => $this->textContent->toArray()
         ], parent::toArray());
